f8db668ab978320dc706aaea0c8ae35b00390933
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-09 17:22:34 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -54,7 +54,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find_by(id: 0)
+  Movie.find_by(id.title: 0)
 end
 
 def can_find_by_multiple_attributes
