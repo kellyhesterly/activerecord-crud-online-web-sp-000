da4bcef80abbd9cc755554b3126386615885b52a
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-09 17:48:15 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -101,7 +101,7 @@ end
 
 def can_destroy_all_items_at_once
   10.times do |i|
-    Movie.create(title: "Movie_#{i}")
+    movie = Movie.create(title: "Movie_#{i}")
   end
-  __
+  movie.destroy_all
 end
