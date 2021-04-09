6fc226b751d1d2758642ad002063b4d0eab1486f
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-09 17:04:20 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,7 +24,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = {:title => attributes[0], :release_date => attributes[1], :director => attributes[2], :lead => attributes[3], :in_theaters => attributes[4]}
+  movie = {:title => nil, :release_date => nil, :director => nil, :lead => nil, :in_theaters => nil}
 
 end
 
