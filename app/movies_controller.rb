b54405451a3bf07495096a7259f33fc5ef7ade1e
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-09 17:05:21 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -26,6 +26,7 @@ def can_be_created_with_a_hash_of_attributes
   }
   movie = {:title => attributes[0], :release_date => attributes[1], :director => attributes[2], :lead => attributes[3], :in_theaters => attributes[4]}
   movie.save
+  movie
 end
 
 def can_be_created_in_a_block(args = __)
